52abc14f976477a6e8093027f076410791b87b3c
Flavio Pinto <flaviopnt@hotmail.it>
2022-09-01 23:33:53 +0200

    Spinner loading comments

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,47 +1,57 @@
 import { Component } from "react";
 import AddComment from "./AddComment";
 import CommentsList from "./CommentsList";
+import Loading from "./Loading";
 
 class CommentArea extends Component {
-
   state = {
-    comments: []
-  }
+    comments: [],
+    loading: true,
+  };
 
   componentDidMount = () => {
     this.fetchComments();
-  }
+  };
 
   fetchComments = async () => {
     try {
-      let res = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.elementId,
-      {
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEwOTI3MmVkZDY3ODAwMTUwN2Q3NDAiLCJpYXQiOjE2NjIwMzA0NTAsImV4cCI6MTY2MzI0MDA1MH0.W-JydjAeXfAhd553HFq5LuXMB5fpJ_ptw4bgjFz-t60',
+      let res = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" +
+          this.props.elementId,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEwOTI3MmVkZDY3ODAwMTUwN2Q3NDAiLCJpYXQiOjE2NjIwMzA0NTAsImV4cCI6MTY2MzI0MDA1MH0.W-JydjAeXfAhd553HFq5LuXMB5fpJ_ptw4bgjFz-t60",
+          },
         }
-      }
       );
-      if(res.ok) {
+      if (res.ok) {
         let data = await res.json();
-        this.setState({
-          comments: data
-        })
+        setTimeout(() => {
+          this.setState({
+            comments: data,
+            loading: false,
+          });
+        }, 1500);
+        
       } else {
-        alert('Qualcosa è andato storto');
+        this.setState({ loading: false });
+        console.log("Qualcosa è andato storto");
       }
     } catch (error) {
-      alert(error);
+      this.setState({ loading: false });
+      console.log(error);
     }
-  }
+  };
 
   render() {
-    return(
+    return (
       <div>
-        <AddComment bookAsin={this.props.elementId}/>
+        {this.state.loading && <Loading />}
+        <AddComment bookAsin={this.props.elementId} />
         <CommentsList comments={this.state.comments} />
       </div>
-    )
+    );
   }
 }
 
